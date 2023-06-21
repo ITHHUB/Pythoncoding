9158530e366de37ab3c06304e4a29520ad580a8e
ithandmai <135786257+ithandmai@users.noreply.github.com>
2023-06-21 10:05:07 +0700

    Update Helloworld.py
    Update1

--- Helloworld.py
+++ Helloworld.py
@@ -74,26 +74,57 @@
 
 
 
-list=[]
-dict={'subject':'unknown','credit':0,'grade':0}
-Ask=input("Add for subject?(Yes/No):")
-while(Ask=="Yes"):
-    subject=input("select your subject")
-    credit=int(input('Your credict:'))
-    grade=int(input('your grade:'))
-    dict['subject']=subject
-    dict['credit']=credit
-    dict['grade']=grade
-    list.append(dict)
-    Ask=input("Do you want to add more subject?(Yes/No):")
-    list2={'subject':subject,'credit':credit,'grade':grade}
-    list.append(list2)
-Change=input("Do you want to change your grade?(Yes/No):")
-while(Change=="Yes"):
-   Subject=input('Enter your subject:')
-   dict['subject']=Subject
-   Grade=int(input("Your new grade?:"))
-   dict['grade']=Grade
-   print("Subject",dict['subject'])
-   print("Grade",dict['grade'])
+# list=[]
+# dict={'subject':'unknown','credit':0,'grade':0}
+# Ask=input("Add for subject?(Yes/No):")
+# while(Ask=="Yes"):
+#     subject=input("select your subject")
+#     credit=int(input('Your credict:'))
+#     grade=int(input('your grade:'))
+#     dict['subject']=subject
+#     dict['credit']=credit
+#     dict['grade']=grade
+#     list.append(dict)
+#     Ask=input("Do you want to add more subject?(Yes/No):")
+#     list2={'subject':subject,'credit':credit,'grade':grade}
+#     list.append(list2)
+# Change=input("Do you want to change your grade?(Yes/No):")
+# while(Change=="Yes"):
+#    Subject=input('Enter your subject:')
+#    dict['subject']=Subject
+#    Grade=int(input("Your new grade?:"))
+#    dict['grade']=Grade
+#    print("Subject",dict['subject'])
+#    print("Grade",dict['grade'])
+#    Change=input("Do you want to change your grade?(Yes/No):")
+
+
+
+
+# subjects=[]
+# grades=[]
+
+# Ask=input("Add for subject?(Yes/No):")
+# while(Ask=="Yes"):
+#     subject=input("select your subject:")
    
+#     grade=int(input('your grade:'))
+#     subjects.append(subject)
+#     grades.append(grade)
+#     Dict=dict(zip(subjects,grades))
+    
+    
+#     Ask=input("Do you want to add more subject?(Yes/No):")
+#     print(Dict)
+#     print("min",min(Dict,key=Dict.get))
+#     print("max",max(Dict,key=Dict.get))
+    
+# Change=input("Do you want to change your grade?(Yes/No):")
+# while(Change=="Yes"):
+#     Subject=input('Enter your subject:')
+#     Dict['subject']=Subject
+#     Grade=int(input("Your new grade?:"))
+#     Dict['grade']=Grade
+#     print("Subject",Dict['subject'])
+#     print("Grade",Dict['grade'])
+#     Change=input("Do you want to change your grade?(Yes/No):")
