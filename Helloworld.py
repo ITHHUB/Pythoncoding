e8311cd4a0781b6f825b79b5f9eb0d861fd63dfd
ithandmai <135786257+ithandmai@users.noreply.github.com>
2023-07-02 20:47:23 +0700

    Update Helloworld.py
    Tax

--- Helloworld.py
+++ Helloworld.py
@@ -130,27 +130,102 @@
 #     Change=input("Do you want to change your grade?(Yes/No):")
 
 
-bookings=[]
-Mode=input("Enter mode(q-quit,b-booking,c-cancel,p-print booking):")
+# bookings=[]
+# Mode=input("Enter mode(q-quit,b-booking,c-cancel,p-print booking):")
 
-while(Mode!="q"):
- if(Mode=="b"):
-    Name=input("Your name:")
-    checkin=int(input("Enter checkin day:"))
-    checkout=int(input("Enter checkout day:"))
-    roomtype=input("Select your type:")
-    BookingNumber=checkin*checkout
-    booking={"Name":Name,"check in":checkin,"check out":checkout,"roomtype":roomtype,"BookingNumber":BookingNumber}
-    bookings.append(booking)
-    Choice=input("Do you want to booking more?(Yes/No):")    
-    print(bookings)
-    print("Booking success")
- elif(Mode=="c"):
-    BookingNumbers=int(input("Your book number:"))
-    for booking in bookings:
-       if booking['BookingNumber']==BookingNumbers:
-          bookings.remove(booking)
- elif(Mode=="p"):
-    print(bookings)
+# while(Mode!="q"):
+#  if(Mode=="b"):
+#     Name=input("Your name:")
+#     checkin=int(input("Enter checkin day:"))
+#     checkout=int(input("Enter checkout day:"))
+#     roomtype=input("Select your type:")
+#     BookingNumber=checkin*checkout
+#     booking={"Name":Name,"check in":checkin,"check out":checkout,"roomtype":roomtype,"BookingNumber":BookingNumber}
+#     bookings.append(booking)
+#     Choice=input("Do you want to booking more?(Yes/No):")    
+#     print(bookings)
+#     print("Booking success")
+#  elif(Mode=="c"):
+#     BookingNumbers=int(input("Your book number:"))
+#     for booking in bookings:
+#        if booking['BookingNumber']==BookingNumbers:
+#           bookings.remove(booking)
+#  elif(Mode=="p"):
+#     print(bookings)
 
- Mode=input("Enter mode(q-quit,b-booking,c-cancel,p-print booking):")
+#  Mode=input("Enter mode(q-quit,b-booking,c-cancel,p-print booking):")
+
+
+
+# def multiplication_or_sum(num1, num2):
+#     # calculate product of two number
+#     product = num1 * num2
+#     # check if product is less then 1000
+#     if product <= 1000:
+#         return product
+#     else:
+#         # product is greater than 1000 calculate sum
+#         return num1 + num2
+#num1=int(input("num1:"))
+#num2=int(input("num2:"))
+# # first condition
+# result = multiplication_or_sum(20, 30)
+# print("The result is", result)
+
+# # Second condition
+# result = multiplication_or_sum(40, 30)
+# print("The result is", result)
+
+
+# Num=0
+# for i in range(0,10):
+#     Sum1=Num+i
+#     print("Current Number",i,"Previous Number",Sum1)
+    
+# String=input("word:")
+# print("Original String is",String)
+# print("Printing only even index chars")
+
+
+
+
+
+income = int(input("Your income :"))
+salary = income * 12
+tax = 0
+tax1 = 0
+tax2 = 0
+tax3 = 0
+tax4 = 0
+tax5 = 0
+tax6 = 0
+tax7 = 0
+tax8 = 0
+if(income <= 150000):
+    tax1 = 0
+elif(income <= 300000):
+    
+    x1 = income - 150000 
+   
+    tax2 = x1 * 5 / 100
+elif(income<=500000):
+    x2 = income - 300000
+    tax3 = x2*10 / 100
+elif(income<=750000):
+    x3 = income - 500000
+    tax4 = x3*15 / 100
+elif(income<=1000000):
+    x4 = income - 750000
+    tax5 = x4*20 / 100
+elif(income<=2000000):
+    x5 = income - 1000000
+    tax6 = x5*25 / 100
+elif(income<=5000000):
+    x6 = income - 2000000
+    tax7 = x6*30 / 100
+elif(income>500000):
+    x7 = income - 5000000
+    tax8 = x7*35 / 100
+
+
+print("Total tax to pay is", tax+tax1+tax2+tax3+tax4+tax5+tax6+tax7+tax8)
