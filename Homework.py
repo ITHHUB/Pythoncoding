c55c51160ef52521a87a58466e1da4fe6f66085f
ithandmai <135786257+ithandmai@users.noreply.github.com>
2023-07-16 16:51:36 +0700

    Add files via upload

--- Homework.py
+++ Homework.py
@@ -1,14 +1,14 @@
-def is_leap_year(Year):  
+# def is_leap_year(Year):  
  
-  if((Year % 400 == 0)  or  (Year % 100 != 0) and  (Year % 4 == 0)):   
-    print("Given Year is a leap Year");  
+#   if((Year % 400 == 0)  or  (Year % 100 != 0) and  (Year % 4 == 0)):   
+#     print("Given Year is a leap Year");  
    
-  else:  
-    print ("Given Year is not a leap Year")  
+#   else:  
+#     print ("Given Year is not a leap Year")  
   
-Year = int(input("Enter the year: "))  
+# Year = int(input("Enter the year: "))  
  
-is_leap_year(Year)  
+# is_leap_year(Year)  
 
 
 
@@ -26,32 +26,40 @@ is_leap_year(Year)
 
 
 
-# import os
+import os
+from datetime import datetime, timezone, timedelta
 
-# def read_file(file_name):
-#     file_handle = open(file_name,'w')
-#     # print (file_handle.read())
-#     file_handle.write("Leap year?")
-#     file_handle.close()
+
+
+TH_dateTime = datetime.now(timezone(timedelta(hours=+7), 'MST'))
+print("In TH::", TH_dateTime)
+
+
+CA_dateTime = datetime.now(timezone(timedelta(hours=-7), 'MST'))
+print("In CA::", CA_dateTime)
+def read_file(file_name):
+    file_handle = open(file_name,'w')
+   
+    file_handle.write("Convert time from Thailand to Canada :")
+    file_handle.write("Input:")
+    file_handle.close()
     
-# file_dir = os.path.dirname(os.path.realpath('D:/IthXD'))
-# print (file_dir)
-
-# #For accessing the file in the same folder
-# file_name = "Date.txt"
-# read_file(file_name)
-
-# #For accessing the file in a folder contained in the current folder
-# file_name = os.path.join(file_dir, file_name)
-# read_file(file_name)
+file_dir = os.path.dirname(os.path.realpath('D:/IthXD'))
+print (file_dir)
 
 
+file_name = "Time.txt"
+read_file(file_name)
 
-# #For accessing the file inside a sibling folder.
-# file_name = os.path.join(file_dir, file_name)
-# file_name = os.path.abspath(os.path.realpath(file_name))
-# print (file_name)
-# read_file(file_name)
+
+file_name = os.path.join(file_dir, file_name)
+read_file(file_name)
+
+
+file_name = os.path.join(file_dir, file_name)
+file_name = os.path.abspath(os.path.realpath(file_name))
+print (file_name)
+read_file(file_name)
 
 
 
